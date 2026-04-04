8cfe9fada87d5864ac4a9ef2bac245a15e3da80a
Raulspl2020 <Raulspl2020@gmail.com>
2026-04-04 15:36:09 -0500

    fix: register view provider during bootstrap init

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,8 @@ $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
+$app->register(Illuminate\View\ViewServiceProvider::class);
+
 /*
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
